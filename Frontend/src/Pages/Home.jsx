7289c9e5b2ec3f31d8defd9d381064f3371cde1e
SK-WebDeveloper <vermasanjaykumar97@gmail.com>
2024-03-31 17:20:24 +0530

    fix : UI Changed

--- Frontend/src/Pages/Home.jsx
+++ Frontend/src/Pages/Home.jsx
@@ -1,42 +1,97 @@
-import React from "react";
-import { Link, NavLink } from "react-router-dom";
-import { Box, Heading, Button } from "@chakra-ui/react";
-import { ArrowDownIcon } from "@chakra-ui/icons";
+// import React from "react";
+// import { Link, NavLink } from "react-router-dom";
+// import { Box, Heading, Button } from "@chakra-ui/react";
+// import { ArrowDownIcon } from "@chakra-ui/icons";
+// const Home = () => {
+//   return (
+//     <Box>
+//       <Box className="nav">
+//         <Button colorScheme="cyan">
+//           <NavLink to="/signup">Sign up</NavLink>
+//         </Button>
+//         <Heading size="lg" color="lightcoral">
+//         Home Page
+//         </Heading>
+//         <Button colorScheme="cyan">
+//           <NavLink to="/login">Login</NavLink>
+//         </Button>
+//       </Box>
+//       <Box>
+//         <Heading color="tomato" size="3xl" mt="100px">
+//           Welcome to Todos
+//         </Heading>
+//         <Heading size="lg" mt="50px">
+//           If you are already login then click{" "}
+//           <span style={{ color: "tomato" }}>
+//             Below Button <ArrowDownIcon />
+//           </span>{" "}
+//           to see <br />{" "}
+//           <Heading color="teal" mt="20px">
+//             YOUR TODOS
+//           </Heading>
+//         </Heading>
+//       </Box>
+//       <Box mt="20px">
+//         <Button colorScheme="cyan">
+//           <Link to="/todos">Todos</Link>
+//         </Button>
+//       </Box>
+//     </Box>
+//   );
+// };
+
+// export default Home;
+
+
+import React from 'react';
+import { Button, Flex, Heading, Link, Text } from '@chakra-ui/react';
+import { Link as RouterLink } from 'react-router-dom';
+import { FiEye, FiUserPlus, FiLogIn } from 'react-icons/fi';
+
 const Home = () => {
   return (
-    <Box>
-      <Box className="nav">
-        <Button colorScheme="cyan">
-          <NavLink to="/signup">Sign up</NavLink>
-        </Button>
-        <Heading size="lg" color="lightcoral">
-        Home Page
-        </Heading>
-        <Button colorScheme="cyan">
-          <NavLink to="/login">Login</NavLink>
-        </Button>
-      </Box>
-      <Box>
-        <Heading color="tomato" size="3xl" mt="100px">
-          Welcome to Todos
-        </Heading>
-        <Heading size="lg" mt="50px">
-          If you are already login then click{" "}
-          <span style={{ color: "tomato" }}>
-            Below Button <ArrowDownIcon />
-          </span>{" "}
-          to see <br />{" "}
-          <Heading color="teal" mt="20px">
-            YOUR TODOS
-          </Heading>
-        </Heading>
-      </Box>
-      <Box mt="20px">
-        <Button colorScheme="cyan">
-          <Link to="/todos">Todos</Link>
-        </Button>
-      </Box>
-    </Box>
+    <Flex
+      align="center"
+      justify="center"
+      direction="column"
+      h="100vh"
+      bg="gray.100"
+    >
+      <Heading mb={8}>Welcome to My App</Heading>
+      <Button
+        as={RouterLink}
+        to="/todos"
+        colorScheme="teal"
+        size="lg"
+        mb={4}
+        leftIcon={<FiEye />}
+      >
+        See Todos
+      </Button>
+      <Flex>
+        <Link as={RouterLink} to="/signup" mr={4}>
+          <Button colorScheme="blue" size="lg" leftIcon={<FiUserPlus />}>
+            Sign Up
+          </Button>
+        </Link>
+        <Link as={RouterLink} to="/login">
+          <Button colorScheme="green" size="lg" leftIcon={<FiLogIn />}>
+            Login
+          </Button>
+        </Link>
+      </Flex>
+      <Text mt={8} fontSize="sm" color="gray.500">
+        Created by{' '}
+        <Link
+          href="https://www.linkedin.com/in/sanjaykumar-verma-a73349219/"
+          isExternal
+          color="teal.500"
+          textDecoration="underline"
+        >
+          Sanjaykumar Verma
+        </Link>{' '}
+      </Text>
+    </Flex>
   );
 };
 
--- Frontend/src/Pages/User/Signup.jsx
+++ Frontend/src/Pages/User/Signup.jsx
@@ -1,26 +1,31 @@
-import React from "react";
-import { useState } from "react";
-import { signup } from "../../Redux/AuthReducer/action";
+import React, { useState } from "react";
 import { useDispatch } from "react-redux";
-import { useNavigate, useLocation } from "react-router-dom";
+import { useNavigate, useLocation, Link } from "react-router-dom";
+import { signup } from "../../Redux/AuthReducer/action";
 import { USER_SIGNUP_SUCCESS } from "../../Redux/AuthReducer/actionType";
-import { Link } from "react-router-dom";
 import {
   Box,
   Button,
   Flex,
   FormControl,
-  Heading,
+  Grid,
   Input,
+  InputGroup,
+  InputRightElement,
+  Stack,
+  useToast,
 } from "@chakra-ui/react";
+import { FiArrowLeft, FiEye, FiEyeOff, FiUserPlus } from "react-icons/fi";
 
 const Signup = () => {
-  const [first_name, setFirstname] = useState("");
-  const [last_name, setLastname] = useState("");
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [age, setAge] = useState("");
 
+  const [showPassword, setShowPassword] = useState(false);
+  const Toast = useToast();
   const dispatch = useDispatch();
 
   const navigate = useNavigate();
@@ -28,10 +33,14 @@ const Signup = () => {
 
   const comingFrom = location?.state?.from?.pathname || "/login";
 
+  const handleTogglePasswordVisibility = () => {
+    setShowPassword(!showPassword);
+  };
+
   const handleSignup = () => {
     const payload = {
-      first_name,
-      last_name,
+      first_name: firstName,
+      last_name: lastName,
       age,
       email,
       password,
@@ -39,72 +48,119 @@ const Signup = () => {
 
     dispatch(signup(payload)).then((res) => {
       if (res.type === USER_SIGNUP_SUCCESS) {
-        alert("Registration Successfull");
+        Toast({
+          title: "Registration Successful",
+          status: "success",
+          duration: 2000,
+          isClosable: true,
+        });
         navigate(comingFrom, { replace: true });
       }
     });
   };
+
   return (
-    <Box>
-      <Flex justifyContent="space-evenly" p="1%">
-        <Button colorScheme="cyan">
-          <Link to="/">Back to home</Link>
-        </Button>
-        <Button colorScheme="cyan">
-          <Link to="/login">Login</Link>
-        </Button>
-      </Flex>
-      <Heading color="tomato">Signup Here</Heading>
-      <Box w="30%" m="auto" mt="30px">
-        <FormControl>
-          <Box>
-            <Input
-              type="text"
-              placeholder="First Name"
-              required
-              onChange={(e) => setFirstname(e.target.value)}
+    <Grid templateColumns="1fr 1fr" gap={8} p={8}>
+      {/* Left Side - Computer Illustration */}
+      <Box marginTop={"10%"}>
+        <Flex justifyContent="center" alignItems="center" h="100%">
+          <svg
+            version="1.1"
+            id="Layer_1"
+            xmlns="http://www.w3.org/2000/svg"
+            xmlnsXlink="http://www.w3.org/1999/xlink"
+            viewBox="0 0 512 512"
+            xmlSpace="preserve"
+            width="70%"
+            height="100%"
+          >
+            {/* Provided SVG code */}
+            <path
+              style={{ fill: "#53626F" }}
+              d="M297.875,404.473c0-27.115,0-38.318,0-38.318h-68.597c0,0,0,11.203,0,38.318 c0,38.439-52.984,53.837-52.984,53.837h174.564C350.859,458.31,297.875,442.912,297.875,404.473z"
             />
-          </Box>
-          <Box mt="20px">
-            <Input
-              type="text"
-              placeholder="Last Name"
-              required
-              onChange={(e) => setLastname(e.target.value)}
+            <path
+              style={{ fill: "#6E8193" }}
+              d="M512,379.648c0,5.054-4.098,9.152-9.152,9.152H9.152C4.098,388.8,0,384.702,0,379.648V42.188 c0-5.054,4.098-9.152,9.152-9.152h493.693c5.054,0,9.152,4.098,9.152,9.152v337.46H512z"
             />
-          </Box>
-          <Box mt="20px">
-            <Input
-              type="text"
-              placeholder="Age"
-              required
-              onChange={(e) => setAge(e.target.value)}
+            <rect
+              x="30.306"
+              y="62.065"
+              style={{ fill: "#FBF5E2" }}
+              width="451.382"
+              height="272.541"
             />
-          </Box>
-          <Box mt="20px">
-            <Input
-              type="email"
-              placeholder="Email"
-              required
-              onChange={(e) => setEmail(e.target.value)}
-            />
-          </Box>
-          <Box mt="20px">
-            <Input
-              type="password"
-              placeholder="Password"
-              required
-              onChange={(e) => setPassword(e.target.value)}
-            />
-          </Box>
-          <Box mt="20px">
-            <Button colorScheme="cyan" onClick={handleSignup}>
-              Sign up
-            </Button>
-          </Box>
-        </FormControl>
+            {/* You can add more paths or elements for further details */}
+          </svg>
+        </Flex>
       </Box>
-    </Box>
+
+      {/* Right Side - Signup Form */}
+      <Box width="70%" marginTop={"10%"}>
+        <Stack spacing={10}>
+          <Flex justifyContent="space-between" alignItems="center">
+            <Button
+              variant="ghost"
+              colorScheme="cyan"
+              leftIcon={<FiArrowLeft />}
+            >
+              <Link to="/">Back to Home</Link>
+            </Button>
+            <Button colorScheme="cyan" leftIcon={<FiUserPlus />}>
+              <Link to="/login">Login</Link>
+            </Button>
+          </Flex>
+          <Box>
+            <FormControl display={"flex"} flexDirection={"column"} gap={"20px"}>
+              <Input
+                type="text"
+                placeholder="First Name"
+                value={firstName}
+                onChange={(e) => setFirstName(e.target.value)}
+              />
+              <Input
+                type="text"
+                placeholder="Last Name"
+                value={lastName}
+                onChange={(e) => setLastName(e.target.value)}
+              />
+              <Input
+                type="text"
+                placeholder="Age"
+                value={age}
+                onChange={(e) => setAge(e.target.value)}
+              />
+              <Input
+                type="email"
+                placeholder="Email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+              <InputGroup>
+                <Input
+                  type={showPassword ? "text" : "password"}
+                  placeholder="Password"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+               <InputRightElement>
+                  <Button
+                    variant="unstyled"
+                    size={"auto"}
+                    onClick={handleTogglePasswordVisibility}
+                  >
+                    {showPassword ? <FiEyeOff /> : <FiEye />}
+                  </Button>
+                </InputRightElement>
+              </InputGroup>
+              <Button colorScheme="cyan" width="100%" onClick={handleSignup}>
+                Sign up
+              </Button>
+            </FormControl>
+          </Box>
+        </Stack>
+      </Box>
+    </Grid>
   );
 };
 
